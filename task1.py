d6f503bff2b665128f5e4c6e709d1edf0c6aa1a5
Sillyman101 <danilsviridov3344@gmail.com>
2024-08-01 13:48:27 -0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,16 @@
+print ('1st program')
+print (9 ** 0.5 * 5)
+
+print ('2nd program')
+print (9.99 > 9.98 and 1000 != 1001)
+
+print ('3rd program')
+num1 = ('1234')
+num2 = ('5678')
+print (int(num1[1:3])+int(num2[1:3]))
+
+print ('4th program')
+floatgiven1 = ('13.42')
+floatgiven2 = ('42.13')
+print (floatgiven1[0:2] == floatgiven2[3:6] or floatgiven1[3:5] == floatgiven2[0:2])
+
